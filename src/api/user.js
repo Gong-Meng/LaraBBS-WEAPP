7ca0ee5860d2e55351960765b00453c84339e08e
Gong Meng <18588264128@163.com>
2022-07-13 18:06:05 +0800

    authRequest and logout

--- src/api/user.js
+++ src/api/user.js
@@ -3,12 +3,3 @@ import { request, authRequest } from '@/utils/request'
 export function getCurrentUser(data) {
   return authRequest('user')
 }
-
-export function logout(token) {
-  return request('authorizations/current', {
-    method: 'delete',
-    header: {
-      'Authorization': 'Bearer ' + token
-    }
-  })
-}
